db06d4e27490c6b86dd538b0534e8b1f64234958
Cyolune <77187747+Cyolune@users.noreply.github.com>
2021-05-28 17:08:22 +0800

    add project

---
+++ Caves and Lizards/Assets/Scripts/Combat Scripts/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int Initiative = 100;
+    public bool isAlive = true;
+    public bool hasEndedTurn = false;
+}
