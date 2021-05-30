f15253b2d16d01acfb0c6f3f80c95242d37dae5d
FestiveCat <31hanyang@gmail.com>
2021-05-30 08:12:42 +0800

    Prepping git for Milestone 1 Submission

--- Caves and Lizards/Assets/Scripts/NavMeshCharacter.cs
+++ Caves and Lizards/Assets/Scripts/NavMeshCharacter.cs
@@ -7,34 +7,18 @@ public class NavMeshCharacter : MonoBehaviour
 {   
     
     private NavMeshAgent navMeshAgent;
-<<<<<<< HEAD
     [SerializeField] private CombatTime combatScript;
 
     private bool hasCombatStarted = false;
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 747a3663bb9a8ba835bd3578a67ff5306e167789
-    public Grid gridComponent;
->>>>>>> 2594fe09400e5c977cf6a25d93ee384733f4d1fc
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-        // grid = Testgrid.GetComponent<Grid>();
->>>>>>> 747a3663bb9a8ba835bd3578a67ff5306e167789
->>>>>>> 2594fe09400e5c977cf6a25d93ee384733f4d1fc
     }
 
     // Update is called once per frame
     void Update()
-<<<<<<< HEAD
     {   
         
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -43,34 +27,6 @@ public class NavMeshCharacter : MonoBehaviour
         bool isCombatTime = combatScript.isItCombatTime();
 
         if (hitSmth)
-=======
-    {
-<<<<<<< HEAD
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        bool hitSmth = Physics.Raycast(ray, out RaycastHit hitInfo);
-        int xCoordGrid = gridComponent.GetX(hitInfo.point);
-        int yCoordGrid = gridComponent.GetY(hitInfo.point);
-
-        if (hitSmth)
-        {
-            Vector3 gridCentre = gridComponent.GetWorldPositionCentre(xCoordGrid, yCoordGrid);
-            if (Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("Mouse 1 Pressed");
-                navMeshAgent.SetDestination(gridCentre);
-            }
-            if (Input.GetMouseButtonDown(1))
-            {
-                Debug.Log("Mouse 2 Pressed, movement stopped");
-                navMeshAgent.SetDestination(transform.position);
-            }
-        }
-
-
-=======
-        if (Input.GetMouseButtonDown(1))
->>>>>>> 2594fe09400e5c977cf6a25d93ee384733f4d1fc
         {
             if (isCombatTime && !hasCombatStarted)
             {
@@ -93,12 +49,8 @@ public class NavMeshCharacter : MonoBehaviour
                 navMeshAgent.SetDestination(transform.position);
             }
         }
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 747a3663bb9a8ba835bd3578a67ff5306e167789
->>>>>>> 2594fe09400e5c977cf6a25d93ee384733f4d1fc
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Caves-and-Lizards
-A game about caves and lizards, for NUS Orbital 2021
+
+Apollo 11
+
+Caves and Lizards is a turn-based RPG inspired by other games like Divinity 
+Original Sin 2 and Dungeons and Dragons.
+
+Caves and Lizards is a turn-based Role Playing Game, focused on ease of play 
+and accessibility. It was inspired by games like Divinity Original Sin 2 and 
+Dungeons and Dragons, while striving to maintain the authenticity while
+simplifying it down for newcomers to the genre.
+
+MS 1 - Ideation
+We realised that there were many players who were interested in Dungeons and
+Dragons, and wanted to try the game out. However, it was hard and confusing for
+them to start from scratch, given the complexity and extremely user-dependent
+nature of the game. As such, we aim to create a game to simplify the confusing
+elements, offering an intuitive gameplay system that allows new users to pick 
+the game up easily, introducing them to this genre of games.
+
+We propose to have these features:
+- Camera movement system 
+- Player control system, including:
+	- Movement
+	- Attacking / Casting spells
+	- Stats and progression
+	- Gearing
+- Coordinate-based world map
+- Turn-based Combat
+- GUI for display of information
+- Short single-player campaign
+
+By MS 1, we have done the following:
+- Camera movement system (via WASD, Edge Panning, Right click rotate, scroll 
+wheel zoom)
+- Out of combat Player movement (Left click move, Right click cancel movement)
+- Movement via shortest-path
+- Crosshair/marker to denote where to the user is pointing at
+- Simple combat start and stop (Starts a certain distance, stops when either
+parties die)
+
+By MS 2, we plan to do the following:
+- Refined Combat including taking turns and a better combat start/stop system
+- Character Stats, skills
+- GUI component
+- Simple Map Design
+- Simple Character Models, Textures and Animations
+
+By MS 3, we plan to do the following:
+- Refining existing technologies and systems
+- Game Balancing
+- Short Campaign
